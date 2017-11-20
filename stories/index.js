696458272c3561bb3b3e2d18d8367aeb3065533b
MaximilianPichler <maximilian.pichler97@gmail.com>
2017-11-20 16:16:26 +0100

    Test git settings

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
+// import { action } from '@storybook/addon-actions'
 import { muiTheme } from 'storybook-addon-material-ui'
 import SuperSelectField from '../src'
 import welcome from './welcome'
