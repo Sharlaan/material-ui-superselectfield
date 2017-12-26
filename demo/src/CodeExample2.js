50d6822a11d8c4432bee459c7196b0518e952e94
Raphaël Morineau <raphael.morineau@gmail.com>
2017-12-26 15:06:06 +0100

    Release v1.8.0 Added onSelect

--- demo/src/CodeExample2.js
+++ demo/src/CodeExample2.js
@@ -11,22 +11,24 @@ const containerStyle = {
   flex: 1
 }
 
-const displayState = state => state.length
-  ? [...state].map(({ value, label }) => label || value).join(', ')
-  : 'empty state'
+const displayState = state =>
+  state.length
+    ? [...state].map(({ value, label }) => label || value).join(', ')
+    : 'empty state'
 
 class CodeExample extends Component {
   state = {
-    state21: [{ value: 'H' }],
-    state22: [{ value: 'I' }],
-    state23: [{ value: 'G' }]
+    state21: [{ value: 'B' }],
+    state22: [{ value: 'F' }],
+    state23: [{ value: 'G' }],
+    state24: []
   }
 
   handleSelection = (values, name) => this.setState({ [name]: values })
 
   render() {
-    const { state21, state22, state23 } = this.state
-    console.debug('state21', state21, '\nstate22', state22, '\nstate23', state23)
+    const { state21, state22, state23, state24 } = this.state
+    console.debug('state21', state21, '\nstate22', state22, '\nstate23', state23, '\nstate24', state24)
 
     return <section style={containerStyle}>
 
@@ -35,55 +37,72 @@ class CodeExample extends Component {
         <div>State 21: {displayState(state21)}</div>
         <div>State 22: {displayState(state22)}</div>
         <div>State 23: {displayState(state23)}</div>
+        <div>State 24: {displayState(state24)}</div>
       </fieldset>
 
       <div style={{ display: 'flex' }}>
         <SuperSelectField
-          name='state21'
+          name="state21"
           multiple
-          checkPosition='left'
-          hintText='Multiple values'
+          checkPosition="left"
+          hintText="Multiple values"
           onChange={this.handleSelection}
           value={state21}
           style={{ minWidth: 150, marginRight: 40 }}
           elementHeight={[36, 68, 36]}
         >
-          <div value='G'>Option G</div>
-          <div value='H'>Option H super longue</div>
-          <div value='I'>Option I</div>
+          <div value="A">Option A</div>
+          <div value="B">Option B super longue</div>
+          <div value="C">Option C</div>
         </SuperSelectField>
 
         <SuperSelectField
-          name='state22'
+          name="state22"
           multiple
-          checkPosition='right'
+          checkPosition="right"
           unCheckedIcon={null}
-          hintText='Multiple values'
+          hintText="Multiple values"
           onChange={this.handleSelection}
           value={state22}
           style={{ minWidth: 150, marginRight: 40 }}
           elementHeight={[36, 68, 36]}
-          menuCloseButton={<FlatButton label='close' hoverColor={'lightSalmon'} />}
+          menuCloseButton={<FlatButton label="close" hoverColor={'lightSalmon'} />}
         >
-          <div value='G'>Option G</div>
-          <div value='H'>Option H super longue</div>
-          <div value='I'>Option I</div>
+          <div value="D">Option D</div>
+          <div value="E">Option E super longue</div>
+          <div value="F">Option F</div>
         </SuperSelectField>
 
         <SuperSelectField
-          name='state23'
+          name="state23"
           multiple
-          hintText='Multiple values'
+          hintText="Custom close button"
           onChange={this.handleSelection}
           value={state23}
           style={{ minWidth: 150 }}
           elementHeight={[36, 52, 36]}
           menuFooterStyle={{ width: '100%' }}
-          menuCloseButton={<FlatButton label='close' hoverColor={'lightSalmon'} style={{ width: '100%' }} />}
+          menuCloseButton={<FlatButton label="close" hoverColor={'lightSalmon'} style={{ width: '100%' }} />}
         >
-          <div value='G'>Option G</div>
-          <div value='H'>Option H super longue</div>
-          <div value='I'>Option I</div>
+          <div value="G">Option G</div>
+          <div value="H">Option H super longue</div>
+          <div value="I">Option I</div>
+        </SuperSelectField>
+      </div>
+
+      <div style={{ display: 'flex' }}>
+        <SuperSelectField
+          name="state24"
+          multiple
+          checkPosition="left"
+          hintText="Multiple values with onSelect"
+          onSelect={this.handleSelection}
+          value={state24}
+          style={{ minWidth: 150, marginTop: 40 }}
+        >
+          <div value="J">Option J</div>
+          <div value="K">Option K</div>
+          <div value="L">Option L</div>
         </SuperSelectField>
       </div>
 
