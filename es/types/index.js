acdc3e932864081b698e5c25b7215a9b23320b2e
Raphaël Morineau <raphael.morineau@gmail.com>
2018-03-13 11:40:16 +0100

    Remove build folders

--- es/types/index.js
+++
@@ -1,3 +0,0 @@
-export { default as floatingLabelTypes } from './floatingLabelTypes'
-export { default as selectFieldTypes } from './selectFieldTypes'
-export { default as selectionsPresenterTypes } from './selectionsPresenterTypes'
